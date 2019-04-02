9a8a911cef044af7823fae03b060a58dd246cb43
elisamartin <elisamv93@gmail.com>
2019-04-02 15:16:39 +0200

    Day 2 MVP

---
+++ authMiddleware.js
@@ -0,0 +1,9 @@
+function authentication(req, res, next) {
+	if (req.session && req.session.user) {
+		next();
+	} else {
+		res.status(400).json({ message: 'You shall not pass!' });
+	}
+}
+
+module.exports = authentication;
--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const Users = require('./data/users-model');
 const authentication = require('./authMiddleware');
+const session = require('express-session');
+
 const server = express();
 
+const sessionConfig = {
+	name: 'monkey',
+	secret: 'this is a secret',
+	cookie: {
+		maxAge: 1000 * 60 * 60,
+		secure: false
+	},
+	httpOnly: true,
+	resave: false,
+	saveUninitialized: false
+};
+
 server.use(express.json());
+server.use(session(sessionConfig));
 
 server.get('/', (req, res) => {
 	res.send("It's alive!");
@@ -29,6 +44,7 @@ server.post('/api/login', (req, res) => {
 	let { username, password } = req.body;
 	Users.findBy({ username }).first().then((user) => {
 		if (user && bcrypt.compareSync(password, user.password)) {
+			req.session.user = user;
 			res.status(200).json({ message: `Hello ${user.username}!` });
 		} else {
 			res.status(401).json({ message: 'You shall not pass!' });
@@ -38,14 +54,20 @@ server.post('/api/login', (req, res) => {
 
 // [GET] /api/users
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', authentication, (req, res) => {
 	Users.find()
 		.then((users) => {
 			res.status(200).json(users);
 		})
 		.catch((err) => {
-			res.status(500).json({ error: 'Something went wrong.' });
+			res.status(500).json({ error: 'You shall not pass!' });
 		});
+});
+
+server.get('/api/logout', (req, res) => {
+	req.session.destroy();
+	res.send('Bye!');
+	res.end();
 });
 
 const port = process.env.PORT || 5050;
